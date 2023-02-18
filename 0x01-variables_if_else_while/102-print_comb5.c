e990bcdf8f7f33191cd6743faee341c6145330b0
Keith koki <kokikeith@gmail.com>
2023-02-18 13:30:16 -0800

    committed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,19 @@ int main(void)
 	int k;
 	int j;
 
-	for (c = '48'; c <= '57'; c++)
+	for (c = 48; c <= 57; c++)
 	{
-		for (i = '48'; i <= '57'; i++)
+		for (i = 48; i <= 57; i++)
 		{
-			for (k = '48'; k <= '57'; k++)
+			for (k = 48; k <= 57; k++)
 			{
-				for (j = '48'; j <= '57'; j++)
+				for (j = 48; j <= 57; j++)
 				{
-					if (((k + j) > (c + i) && k >= c) || c > k)
+					if (((k + j) > (c + i) && k >= c) || c < k)
 					{
 						putchar(c);
 						putchar(i);
-						putchar(',');
+						putchar(' ');
 						putchar(k);
 						putchar(j);
 
