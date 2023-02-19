da396ad0f02cba808c9f8114da6aa51c2ecfcb51
Keith koki <kokikeith@gmail.com>
2023-02-19 12:37:57 -0800

    committed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,22 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 98; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j <= 98; j++)
+		for (j = i; j <= 99; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
+			int tens_i = i / 10;
+			int ones_i = i % 10;
+			int tens_j = j / 10;
+			int ones_j = j % 10;
 
-			if (j != 98)
+			putchar(tens_i + '0');
+			putchar(ones_i + '0');
+			putchar(' ');
+			putchar(tens_j + '0');
+			putchar(ones_j + '0');
+
+			if (!(i == 99 && j == 99))
 			{
 				putchar(',');
 				putchar(' ');
