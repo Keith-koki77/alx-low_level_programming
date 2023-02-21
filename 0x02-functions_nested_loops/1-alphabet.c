037c2ca64f7fa8a3206e57db5552176c69812b0d
Keith koki <kokikeith@gmail.com>
2023-02-21 13:42:12 -0800

    committed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	_putchar('\n');
+}
