0170a99b154f809aad338df14aebbc1ae9100415
Keith koki <kokikeith@gmail.com>
2023-02-21 11:27:18 -0800

    main.h is updated

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+extern int_putchar(charc);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int_islower(int c);
+extern int_isalpha(int c);
+extern int print_sign(int n);
+extern int_abs(int);
+extern int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_tims_table(int n);
+
+#endif
