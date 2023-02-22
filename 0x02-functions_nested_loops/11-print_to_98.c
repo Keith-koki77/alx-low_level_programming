60d2b7ea63cd6cbf3afda8cfdf31079d56a2d2c0
Keith koki <kokikeith@gmail.com>
2023-02-21 23:48:53 -0800

    committrd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,18 +15,18 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d\n", i);
+				printf("%d\n, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
 		}
 	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
 		{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					printf("%d\n", j);
-				else if (j == 98)
-					printf("%d\n", j);
-			}
+			if (j != 98)
+				printf("%d\n, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
+	}
 }
