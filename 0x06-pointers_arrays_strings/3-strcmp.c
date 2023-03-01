950d8501074284f9710d966aeaeb54ecf7d4faa1
Keith koki <kokikeith@gmail.com>
2023-03-01 14:14:35 -0800

    committed strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,23 @@
 
 /**
  * _strcmp - function that compares string values
- * @si: input first value
- * @s2: input second value
+ * @s1: input value
+ * @s2: input value
  *
- * Return: s1[a] - s2[a]
+ * Return: s1[x] - s2[x]
  */
 int _strcmp(char *s1, char *s2)
 {
 	int x;
 
 	x = 0;
-	while (s1[a] != '\0' && s2[a] != '\0')
+	while (s1[x] != '\0' && s2[x] != '\0')
 	{
-		if (s1[a] != s2[a])
+		if (s1[x] != s2[x])
 		{
-			return (s1[a] - s2[a]);
+			return (s1[x] - s2[x]);
 		}
-		a++;
+		x++;
 	}
 	return (0);
 }
