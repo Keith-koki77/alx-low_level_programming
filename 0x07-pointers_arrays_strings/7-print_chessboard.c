043ff8781b3538235bfb350c29c542f9967ca84c
Keith koki <kokikeith@gmail.com>
2023-03-06 07:16:19 -0800

    committed 7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			printf("%c ", a[x][y]);
+		}
+		printf("\n");
+	}
+}
