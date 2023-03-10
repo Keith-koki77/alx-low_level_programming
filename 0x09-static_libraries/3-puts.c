47b4a54f7b1f86d80e3edd6403be5cf21f269929
Keith koki <kokikeith@gmail.com>
2023-03-10 13:39:44 -0800

    initial commit

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - used to write a line or string to the output
+ * @s: input
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
