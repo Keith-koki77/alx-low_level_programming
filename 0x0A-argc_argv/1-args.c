8b8d7ff9fe8b284c2a60af99335eddef25edc3d9
Keith koki <kokikeith@gmail.com>
2023-03-12 13:19:52 -0700

    committted nargs

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,17 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the number of arguments passed to it
- * @argc: counts the arguments
+ * @argc: counts argumnets
  * @argv: arguments
  *
- * Return: 0
+ * Return: 0 (succes)
  */
 int main(int argc, char *argv[])
 {
 	(void)argv; /*ignore argv*/
-	
+
 	printf("%i\n", argc - 1);
 
 	return (0);
