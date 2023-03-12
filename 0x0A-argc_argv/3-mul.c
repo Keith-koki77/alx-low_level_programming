3f82c1da7f845691f4ab35b655171462c3504b4f
Keith koki <kokikeith@gmail.com>
2023-03-12 14:15:53 -0700

    committed mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: counts the arguments
+ * @argv: the arguments
+ *
+ * Return: 0 if true, 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int result;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	result = x * y;
+
+	printf("%d\n", result);
+
+	return (0);
+}
