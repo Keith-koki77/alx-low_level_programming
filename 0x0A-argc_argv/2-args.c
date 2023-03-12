bc0237c7b7e24d85a08b28c65c2b8eede5400d3f
Keith koki <kokikeith@gmail.com>
2023-03-12 13:39:17 -0700

    initial commit args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: counts all arguments
+ * @argv; arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+
+	return (0);
+}
