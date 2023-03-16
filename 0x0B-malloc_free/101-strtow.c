d0e0b1c8df51f86f79bf3ed7b43539838900d3e0
Keith koki <kokikeith@gmail.com>
2023-03-16 03:35:15 -0700

    committed strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,38 +1,71 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: nothing
+ * Return: number of words
  */
-void print_tab(char **tab)
+int count_word(char *s)
 {
-    int i;
+	int c, flag = 0, w = 0;
 
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+	return (w);
 }
-
 /**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
-int main(void)
+* **strtow - splits a string into words
+* @str: string to split
+*
+* Return: pointer to an array of strings (Success)
+* or NULL (Error)
+*/
+char **strtow(char *str)
 {
-    char **tab;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-    tab = strtow("      ALX School         #cisfun      ");
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    print_tab(tab);
-    return (0);
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+			end = i;
+			tmp = (char *) malloc(sizeof(char) * (c + 1));
+			if (tmp == NULL)
+				return (NULL);
+			while (start < end)
+				*tmp++ = str[start++];
+			*tmp = '\0';
+			matrix[k] = tmp - c;
+			k++;
+			c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+	matrix[k] = NULL;
+	return (matrix);
 }
