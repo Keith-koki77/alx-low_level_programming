7bf770de75d3f5fdd41019df61ecbe62846a8d06
Keith koki <kokikeith@gmail.com>
2023-05-22 10:00:46 -0700

    first commit

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int mod(int a, int b)
+{
+    printf("%d\n", a %  b);
+
+}
+
+int sub(int a, int b)
+{
+    printf("%d\n", a - b);
+}
+
+int mul(int a, int b)
+{
+    printf("%d\n", a *  b);
+
+}
+
+int div(int a, int b)
+{
+    printf("%d\n", a /  b);
+}
+
+int add(int a, int b)
+{
+    printf("%d\n", a +  b);
+}
