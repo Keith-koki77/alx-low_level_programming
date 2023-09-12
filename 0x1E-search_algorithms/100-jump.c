c606e19b0e0b774d2bba5d8385070af9804a625e
Keith koki <kokikeith@gmail.com>
2023-09-12 04:39:30 -0700

    file fixed

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,30 +13,34 @@
 int jump_search(int *array, size_t size, int value)
 {
 	int step;
-	int left;
-	int right;
+	int prev;
 	int i;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	step = (int)sqrt(size);
-	left = 0;
-	right = step;
+	step = 0;
+	prev = 0;
+	i = 0;
 
-	while (right < (int)size && array[right] < value)
+	while (step < (int)size && array[step] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", right, array[right]);
-		left = right;
-		right += step;
+		printf("Value checked array[%i] = [%i]\n", step, array[step]);
+		prev = step;
+		step += (int)(sqrt(size));
+		if (prev >= (int)size)
+			return (-1);
 	}
 
-	printf("Value found between indexes [%d] and [%d]\n", left, right);
 
-	for (i = left; i <= right && i < (int)size; i++)
+	printf("Value found between indexes [%d] and [%d]\n", prev, step);
+
+	if (step > (int)(size - 1))
+		step = (int)size - 1;
+
+	for (i = prev; i <= step && array[i] <= value; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
